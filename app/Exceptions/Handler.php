58bb8e9870417c94b4173bb2a29893b0ca73a54b
Robson de Jesus <robson.djsilva@gmail.com>
2021-11-27 00:49:16 -0300

    🔨  Added docker environment

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -38,4 +38,25 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Throwable $exception)
+    {
+        if($request->is("api/*")){
+            if($exception instanceof ValidationException){
+                return response()->json(
+                    $exception->errors(),
+                    $exception->status
+                );
+            }
+        }
+
+        return parent::render($request, $exception);
+    }
 }
